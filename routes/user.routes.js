5c7880589bb1e7cbbf35ee304ff647b477854688
Milan Vicentijevic <jaiunequestion@hotmail.com>
2025-06-16 15:14:30 +0200

    update works

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/userController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.get('/', controller.getAllUsers);
-router.get('/:id', controller.getUserById);
-router.post('/', controller.createUser);
-router.put('/:id', controller.updateUser);
-router.delete('/:id', controller.deleteUser);
+router.get('/:id',  controller.getUserById);
+router.post('/', authMiddleware.verifyToken, controller.createUser);
+router.put('/:id', authMiddleware.verifyToken, controller.updateUser);
+router.delete('/:id', authMiddleware.verifyToken, controller.deleteUser);
 
 module.exports = router;
