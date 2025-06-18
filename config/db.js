4067f4260356d461358ccee12e4f79f52065d340
Milan Vicentijevic <jaiunequestion@hotmail.com>
2025-06-18 12:11:23 +0200

    Mise en prod

--- config/db.js
+++ config/db.js
@@ -13,4 +13,14 @@ const sequelize = new Sequelize(
   }
 );
 
+connection.connect((err) => {
+    if (err) {
+        console.error('Erreur de connexion à la base de données :', err);
+        return;
+    }
+    console.log('Connexion à la base de données réussie !');
+});
+
 module.exports = sequelize;
+
+
